3842bbd30058f19ed1a8846712ecfaa336cee906
EndymionSpr <endymion1@vp.pl>
2022-07-08 23:39:30 +0200

    Added example of .env file and some code cleanup in index.js

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 require('dotenv').config()
-const {Client, Intents} = require('discord.js')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES]});
+const { Client, Intents } = require('discord.js')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-client.on('ready', () =>{
+client.on('ready', () => {
     console.log(`logged in as ${client.user.tag}!`)
 })
 
-client.on('messageCreate',  (message) => {
+client.on('messageCreate', (message) => {
 
     const Message = message.content;
 
-    if(Message.includes('_460svav1.')){
-        const correctMessage = Message.replace("_460svav1.mp4","_460sv.mp4");
+    if (Message.includes('_460svav1.')) {
+        const correctMessage = Message.replace("_460svav1.mp4", "_460sv.mp4");
         message.reply({
             content: correctMessage,
         })
-    } 
     }
-)
+})
 
 client.login(process.env.DISCORD_TOKEN)
